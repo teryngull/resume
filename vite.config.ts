bdce048c96709ba5c1f3a2786d97fdd9e9272780
teryngull <teryngull@me.com>
2025-10-13 21:25:32 -0600

    add mui and workflow

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,19 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  base: "/resume/",
-  plugins: [react()],
-})
+export default defineConfig(({ command }) => {
+  const config = {
+      plugins: [react()],
+      base: '/',
+      server: {
+        open: true,
+        port: 5500,
+      },
+  };
+
+  if (command !== 'serve') {
+      config.base = '/resume/';
+  }
+
+  return config;
+});
